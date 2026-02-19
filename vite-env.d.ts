3ce24e3b49f7316c8ebc9321aaaa67256b8c50c6
flokisak <floki@jobshaman.cz>
2026-02-19 19:48:39 +0100

    mailing list

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
   readonly VITE_SUPABASE_KEY: string
   readonly VITE_BACKEND_URL?: string
+  readonly VITE_SEARCH_API_URL?: string
   readonly VITE_SEARCH_BACKEND_URL?: string
   readonly VITE_SEARCH_V2_ENABLED?: string
   readonly VITE_ENABLE_VERCEL_ANALYTICS?: string
